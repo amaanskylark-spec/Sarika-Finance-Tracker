6b7c88609fe02e7a96ec600e35cc8cdf6ddaf1d2
amaanskylark-spec <amaanskylark@gmail.com>
2026-03-29 23:53:44 +0530

    Remove generateStaticParams function due to conflict with 'use client'

---
+++ app/dashboard/person/[id]/page.tsx
@@ -0,0 +1,7 @@
+// Removed generateStaticParams function due to conflict with 'use client'
+
+import { useEffect } from 'react';
+
+export default function Page({ params }: { params: { id: string } }) {
+  // Functionality to render the page based on params.id
+}
